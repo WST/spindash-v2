b90dd7e4002f0b0a2dc9eaad8249919dd0b54eca
WST <ilya.averkov@gmail.com>
2016-04-26 17:13:45 +0300

    Send proper 404 code: fix

--- core/SpinDash/Http/Response.php
+++ core/SpinDash/Http/Response.php
@@ -13,7 +13,7 @@ namespace SpinDash\Http;
 
 final class Response
 {
-	private $status_code = 200;
+	private $code = 200;
 	private $body = '';
 
 	public function setBody($data) {
@@ -21,21 +21,21 @@ final class Response
 	}
 
 	public function statusCodeDescription() {
-		switch($this->status_code) {
+		switch($this->code) {
 			case 404: return 'Not Found'; break;
 			default: return 'OK'; break;
 		}
 	}
 
 	public function sendBasic() {
-		if($this->status_code !== 200) {
-			header("HTTP/1.1  {$this->status_code} " . $this->statusCodeDescription());
+		if($this->code !== 200) {
+			header("HTTP/1.1  {$this->code} " . $this->statusCodeDescription());
 		}
 		echo $this->body;
 	}
 
 	public function sendPHPSGI() {
-		return ["{$this->status_code} " . $this->statusCodeDescription(), ['Content-Type' => 'text/html'], $this->body];
+		return ["{$this->code} " . $this->statusCodeDescription(), ['Content-Type' => 'text/html'], $this->body];
 	}
 
 	public function setStatusCode($code) {
